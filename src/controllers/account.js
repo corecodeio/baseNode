d11162b6b1198ae1a8e7fcca4c6c0e2aaea05d9b
Yosef Maldonado Arriaga <yosef.maldonado@pccenter.com.gt>
2021-10-16 11:29:01 -0600

    last changes

--- src/controllers/account.js
+++ src/controllers/account.js
@@ -1,7 +1,11 @@
 const Account = require('../models/account');
 
 module.exports.createAccount =  (req, res, next) => {
-  res.status(200).json({ valid: true, message: 'createAccount' })
+  const args = [req.body.description, req.body.account_type, req.body.currency, req.user.uid];
+  console.log(args);
+  Account.create(args)
+  .then(() => res.status(200).json({ valid: true, message: 'Account created!' }))
+  .catch((e) => res.status(400).json({ valid: false, message: e }));
 }
 
 module.exports.getAccount = (req, res, next) => {
@@ -9,5 +13,8 @@ module.exports.getAccount = (req, res, next) => {
 }
 
 module.exports.getAccounts = (req, res, next) => {
-  res.status(200).json({ valid: true, message: 'getAccounts' })
+  const args = [req.user.uid];
+  Account.fetchAll(args)
+  .then(({rows}) => res.status(200).json({ valid: true, data: rows }))
+  .catch((e) => res.status(400).json({ valid: false, message: e }));
 }
